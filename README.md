69919564c2200e84db0f49555ee49a5e4566d7aa
cristianrs-dev <cris.tecwordd@gmail.com>
2025-05-23 10:23:54 -0400

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jogo-spacer-invaders
---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1748010002,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/space.png",
+		"images/nave-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/alien-sheet0.png",
+		"images/alien2-sheet0.png",
+		"images/alien3-sheet0.png",
+		"images/naveinimiga-sheet0.png",
+		"images/wellcome-sheet0.png",
+		"images/start-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
